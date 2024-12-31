d1539d93691a41ec50c89e7a3d2489c38201e518
shanmukha <shanuthanudevkam@gmail.com>
2025-01-01 00:05:45 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,5 @@
+function newYear(){
+    document.getElementById("button").style.backgroundColor = 'rgb(65, 113, 195)';
+    document.getElementById("button").style.color = 'white';
+    // windows.location.replace("http://127.0.0.1:5500/mainPage.html");
+}
