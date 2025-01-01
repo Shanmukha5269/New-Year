72546aab94e96c8596232f3fb090bd259221fe0c
shanmukha <shanuthanudevkam@gmail.com>
2025-01-01 16:11:18 +0530

    Added audio and video

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function newYear(){
-    document.getElementById("button").style.backgroundColor = 'rgb(65, 113, 195)';
-    document.getElementById("button").style.color = 'white';
-    // windows.location.replace("http://127.0.0.1:5500/mainPage.html");
-}
+// function newYear(){
+//     document.getElementById("button").style.backgroundColor = 'rgb(65, 113, 195)';
+//     document.getElementById("button").style.color = 'white';
+//     // windows.location.replace("http://127.0.0.1:5500/mainPage.html");
+// }
+
+const audio = new Audio('firework_audio.mp3');
+setTimeout(() =>{
+    audio.play();
+},1500);
