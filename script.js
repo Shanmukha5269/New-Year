2243cd538984b456a7524b61aa49098d410ce313
shanmukha <shanuthanudevkam@gmail.com>
2025-04-17 18:04:13 +0530

    Changes from enhanced_ui branch

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// function newYear(){
-//     document.getElementById("button").style.backgroundColor = 'rgb(65, 113, 195)';
-//     document.getElementById("button").style.color = 'white';
-//     // windows.location.replace("http://127.0.0.1:5500/mainPage.html");
-// }
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const audio = new Audio('firework_audio.mp3');
-setTimeout(() =>{
-    audio.play();
-},1500);
+// const audio = new Audio('firework_audio.mp3');
+// setTimeout(() =>{
+//     audio.play();
+// },1500);
+
+app.use(express.static(path.join(__dirname)));
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, "index.html"))
+} )
+
+app.listen(8000, "0.0.0.0" ,() =>{
+    console.log("Server is running in http://localhost:8000");
+});
